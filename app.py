e3594ee9fb79fe7d4752d853e57b11236691e9a4
Anja Huhmer <ahuehmer@gmail.com>
2023-04-16 18:13:35 -0700

    App progress

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
 Base.prepare(autoload_with=engine)
 
@@ -32,17 +33,20 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return (
-        f"Welcome to Hawaii Weather App<br/>"
-        f"Available Routes:<br/>"
+        f"Welcome to Hawaii Weather App<br/><br/>"
+        f"Available Routes:<br/><br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/<start>/<end><br/><br/>"
+        f" Please enter start and end dates in a yyyy-mm-dd format"
     )
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    
     # Create our session (link) from Python to the DB
     session = Session(engine)
     
@@ -54,13 +58,77 @@ def precipitation():
     #Close session
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
+    # Create a dictionary 
+    precipitation = []
     for date, prcp in date_prcp:
-        passenger_dict = {date: prcp}
-        all_passengers.append(passenger_dict)
+        precipitation_dict = {date: prcp}
+        precipitation.append(precipitation_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(precipitation)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query station data
+    station_data = session.query(station.station, station.name).all()
+    
+    #Close session
+    session.close()
+    
+    # Create a dictionary 
+    station_list = []
+    for stat, name in station_data:
+        station_dict = {stat: name}
+        station_list.append(station_dict)
+
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def temperature():
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query temperature data
+    recent_date = dt.date(2017, 8 , 23)
+    year_ago = recent_date - dt.timedelta(days=365)
+    date_tobs = session.query(measurement.date, measurement.tobs).filter(measurement.date >= year_ago).\
+    filter(measurement.station == "USC00519281").group_by(measurement.date).all() 
+    
+    #Close session
+    session.close()
+    
+    # Create a dictionary 
+    temperature = []
+    for date, tobs in date_tobs:
+        temperature_dict = {date: tobs}
+        temperature.append(temperature_dict)
+
+    return jsonify(temperature)
+
+
+
+@app.route("/api/v1.0/<start>")
+def temp_by_start_date(start):
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    #Query necessary data
+    sel = [func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)]
+    temp_by_start_date = session.query(*sel).filter(measurement.date >= start).all()
+
+    temp_by_start_date_list = []
+    for temperature in temp_by_start_date:
+        temp_by_start_date_dict = {"Min": temperature[0], "Max": temperature[1], "Avg": temperature[2]}
+        temp_by_start_date_list.append(temp_by_start_date_dict)
+
+    return jsonify(temp_by_start_date_list)
+
 
 
 if __name__ == "__main__":
